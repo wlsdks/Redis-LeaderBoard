9352cdfe23e1a2f06a7bae36eb3f42f38349ef77
jinan <wlsdks12@naver.com>
2023-06-21 08:30:22 +0900

    1.first commit

---
+++ src/test/java/study/redis/LeaderBoard/LeaderBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package study.redis.LeaderBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaderBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
